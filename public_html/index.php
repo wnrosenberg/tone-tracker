9c1ef1c968a167f9b34b1119b2199829ecf1b8b2
Will Rosenberg <will.rosenberg+response@gmail.com>
2014-11-22 15:15:37 -0500

    useful links

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,9 @@
 	<ul>
 		<li><a href="./basico.php">Basic Oscillator Demo</a></li>
 	</ul>
+	<p>For more info, check out:<br>
+		<a href="http://updates.html5rocks.com/2012/01/Web-Audio-FAQ">HTML5Rocks Web Audio FAQ</a><br>
+		<a href="https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html">W3C Web Audio Specification</a></p>
 <?php
 	include("footer.php");
 
