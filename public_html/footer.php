863d6fa32aa04a5ff58ddb04fcc0b1e7e12103b2
Will Rosenberg <will.rosenberg+response@gmail.com>
2014-11-25 17:14:19 -0500

    piano plays!

--- public_html/footer.php
+++ public_html/footer.php
@@ -5,6 +5,7 @@
 	<!-- For now, _app.js is fine, but in production, you should compile this to scripts.min.js. -->
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script type="text/javascript" src="./assets/scripts/_app.js"></script>
+	<script type="text/javascript" src="./assets/scripts/piano.js"></script>
 </body>
 </html>
 
--- public_html/osc_piano.php
+++ public_html/osc_piano.php
@@ -9,11 +9,18 @@
 	<fieldset id="pianokeys" class="clearfix">
 		<legend>Piano Keys</legend>
 
+		<div class="key ghost" data-note="   " data-marker="start"></div>
+
+		<div class="key ghost" data-note="   " data-keyboard="Q"></div>
+
 		<div class="key" data-note="C-4" data-keyboard="A"></div>
 		<div class="key" data-note="C#4" data-keyboard="W"></div>
 		<div class="key" data-note="D-4" data-keyboard="S"></div>
 		<div class="key" data-note="D#4" data-keyboard="E"></div>
 		<div class="key" data-note="E-4" data-keyboard="D"></div>
+
+		<div class="key ghost" data-note="   " data-keyboard="R"></div>
+
 		<div class="key" data-note="F-4" data-keyboard="F"></div>
 		<div class="key" data-note="F#4" data-keyboard="T"></div>
 		<div class="key" data-note="G-4" data-keyboard="G"></div>
@@ -21,62 +28,34 @@
 		<div class="key" data-note="A-4" data-keyboard="H"></div>
 		<div class="key" data-note="A#4" data-keyboard="U"></div>
 		<div class="key" data-note="B-4" data-keyboard="J"></div>
+
+		<div class="key ghost" data-note="   " data-keyboard="I"></div>
+
 		<div class="key" data-note="C-5" data-keyboard="K"></div>
 		<div class="key" data-note="C#5" data-keyboard="O"></div>
 		<div class="key" data-note="D-5" data-keyboard="L"></div>
 		<div class="key" data-note="D#5" data-keyboard="P"></div>
-		<div class="key" data-note="E-5" data-keyboard=";"></div>
+		<div class="key" data-note="E-5" data-keyname="colon" data-keyboard=";"></div>
+
+		<div class="key ghost" data-note="   " data-marker="end"></div>
 
 	</fieldset>
 
 	<fieldset id="controls">
 		<legend>Controls</legend>
 
-		<div class="buttons">
-			<button id="start">Connect Output</button>
-			<button id="stop">Disconnect Output</button>
-		</div>
-
-
 		<fieldset>
-			<legend>Oscillator [SOURCE]</legend>
-			<figure class="icon">
-				<img src="./assets/images/oscillator_icon.png">
-			</figure>
-			<div>
-				<label><span>Frequency:</span> <input id="ofreq" type="range" value="440" min="220" max="880" step="36.6666666666666" /></label><input type="text" class="range_value" data-round="1" value="440" />
-			</div>
-		</fieldset>
-
-
-		<fieldset>
-			<legend>Gain [EFFECT]</legend>
-			<figure class="icon">
-				<img src="./assets/images/effects_icon.png">
-			</figure>
-			<div>
-				<label><span>Volume:</span> <input id="gvol" type="range" value="0.50" min="0.00" max="1.00" step="0.05" /></label><input type="text" class="range_value" data-round=".01" value="0.50" />
-			</div>
-		</fieldset>
-
-		<fieldset>
-			<legend>Speakers [DESTINATION]</legend>
+			<legend>Master Volume</legend>
 			<figure class="icon">
 				<img src="./assets/images/speaker_icon.png">
 			</figure>
-			<div></div>
+			<div>
+				<label><span>Volume:</span> <input id="gvol" type="range" value="0.75" min="0.00" max="1.00" step="0.05" /></label><input type="text" class="range_value" data-round=".01" value="0.75" />
+			</div>
 		</fieldset>
-			
-
-
+		
 	</fieldset>
 	
-	<script type="text/javascript">
-		window.onload = function() {
-  			osc_piano_init();
-		};
-	</script>
-
 <?php
 	include("footer.php");
 
