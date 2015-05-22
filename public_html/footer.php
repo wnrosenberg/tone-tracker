7df4466ccc645124c04c1a8e3d50330a5cde0aab
Will Rosenberg <will.rosenberg+mbp@gmail.com>
2015-05-21 20:15:10 -0400

    added jquery (1.7.1) from a local version. i think the current is like 1.12.x?

--- public_html/footer.php
+++ public_html/footer.php
@@ -3,7 +3,9 @@
 	</main>
 
 	<!-- For now, _app.js is fine, but in production, you should compile this to scripts.min.js. -->
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<!--script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script-->
+	<script type="text/javascript" src="./assets/scripts/jquery.min.js"></script>
+	<? /* 1.7.1 for now */ ?>
 	<script type="text/javascript" src="./assets/scripts/_app.js"></script>
 	<script type="text/javascript" src="./assets/scripts/piano.js"></script>
 </body>
